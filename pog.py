38fc0e3407e0e03ac780bca45d0b6d756e979193
Amartya Giri <AmartyaWasTaken@gmail.com>
2025-07-22 08:52:25 -0700

    Pogged the variables attempt 2

--- pog.py
+++ pog.py
@@ -1,2 +1,8 @@
-Krishna = 11920
-print(Krishna)
+# integer refers to a number without decimals
+Krishnas_value_in_relation_to_a_meter_ranging_from_0_to_10000000000000 = 11920
+# float is a pog number with decimals
+ReDay_the_king_himselfs_value_in_relation_to_0_to_10000000000000 = 111992200.99
+Individual_is_delusional = True  # boolean means True or False
+Individual_is_delusional = False
+print(Krishnas_value_in_relation_to_a_meter_ranging_from_0_to_10000000000000 >
+      ReDay_the_king_himselfs_value_in_relation_to_0_to_10000000000000)
