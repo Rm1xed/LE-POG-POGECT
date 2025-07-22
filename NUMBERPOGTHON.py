7411970ac905241336ff6d2266336a69353142b0
Amartya Giri <AmartyaWasTaken@gmail.com>
2025-07-22 16:22:19 -0700

    Numbers&WorkingW/Them

---
+++ NUMBERPOGTHON.py
@@ -0,0 +1,10 @@
+print(19 ** 17)
+print(20 % 5) #divides the two then tells us the remainder
+print(25 // 25) #for an integer (whole number) answer
+x = 67.5
+x += 15
+import math
+round(1297498124.92)
+print(abs(-2355.8))
+
+print(math.ceil(2.9))
