7ae55135706686a299d496fc3c6153debb67b3d1
Amartya Giri <AmartyaWasTaken@gmail.com>
2025-07-22 16:57:36 -0700

    ConditionalStatements + type conversion

---
+++ ConitionalPogs.py
@@ -0,0 +1,9 @@
+temperature = 25
+if temperature > 50:
+    print("it's fucking hot ash")
+    print("drink some focking wotah m8")
+elif temperature > 35:
+    print("It's gorgeous!")
+else:
+    print("focking cold ash")
+print("Pog")
